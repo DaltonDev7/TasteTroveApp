2f3bee502afb4605c8d88d5403e9c79cf9cbf70b
DaltonDev7 <daltontejada28@gmail.com>
2024-09-01 20:00:00 -0400

    feat: importando firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { firebaseProviders } from './core/config/firebase.config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes), 
+    provideClientHydration(),
+    
+    firebaseProviders],
+
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig:{
+        apiKey: "AIzaSyDWY_o59RTdd1eQ7-g8oCyhNIwfxloKuN0",
+        authDomain: "note-taste-trove.firebaseapp.com",
+        projectId: "note-taste-trove",
+        storageBucket: "note-taste-trove.appspot.com",
+        messagingSenderId: "972466161362",
+        appId: "1:972466161362:web:d3facf6a048bafa08f0f0b"
+    }
+};
